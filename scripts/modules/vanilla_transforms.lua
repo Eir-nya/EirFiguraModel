8a695947a818edb348711132f946b91bed98f2ef
Eir-nya <Eir.nyanbinary@gmail.com>
2023-03-20 16:32:48 -0500

    Improve vanilla transforms rendering

--- scripts/modules/vanilla_transforms.lua
+++ scripts/modules/vanilla_transforms.lua
@@ -29,21 +29,31 @@ end
 modules.events.RENDER:register(vt.render)
 
 function vt.postRender(delta, ctx)
-	if ctx ~= "RENDER" then
-		return
+	if ctx == "RENDER" then
+        vt.restorePart(models.cat.Head, vanilla_model.HEAD, vanilla_model.HAT)
+        vt.restorePart(models.cat.Body, vanilla_model.BODY, vanilla_model.JACKET)
+        vt.restorePart(models.cat.LeftArm, vanilla_model.LEFT_ARM, vanilla_model.LEFT_SLEEVE)
+        vt.restorePart(models.cat.RightArm, vanilla_model.RIGHT_ARM, vanilla_model.RIGHT_SLEEVE)
+        vt.restorePart(models.cat.LeftLeg, vanilla_model.LEFT_LEG, vanilla_model.LEFT_PANTS)
+        vt.restorePart(models.cat.RightLeg, vanilla_model.RIGHT_LEG, vanilla_model.RIGHT_PANTS)
+
+        if previous.elytra then
+            vt.restorePart(models.cat.Body.Elytra.left, vanilla_model.LEFT_ELYTRA)
+            vt.restorePart(models.cat.Body.Elytra.right, vanilla_model.RIGHT_ELYTRA)
+        end
+    else
+        vt.restoreVanillaParts(vanilla_model.HEAD, vanilla_model.HAT)
+        vt.restoreVanillaParts(vanilla_model.BODY, vanilla_model.JACKET)
+        vt.restoreVanillaParts(vanilla_model.LEFT_ARM, vanilla_model.LEFT_SLEEVE)
+        vt.restoreVanillaParts(vanilla_model.RIGHT_ARM, vanilla_model.RIGHT_SLEEVE)
+        vt.restoreVanillaParts(vanilla_model.LEFT_LEG, vanilla_model.LEFT_PANTS)
+        vt.restoreVanillaParts(vanilla_model.RIGHT_LEG, vanilla_model.RIGHT_PANTS)
+
+        if previous.elytra then
+            vt.restoreVanillaParts(vanilla_model.LEFT_ELYTRA)
+            vt.restoreVanillaParts(vanilla_model.RIGHT_ELYTRA)
+        end
 	end
-
-    vt.restorePart(models.cat.Head)
-    vt.restorePart(models.cat.Body)
-    vt.restorePart(models.cat.LeftArm)
-    vt.restorePart(models.cat.RightArm)
-    vt.restorePart(models.cat.LeftLeg)
-    vt.restorePart(models.cat.RightLeg)
-
-    if previous.elytra then
-        vt.restorePart(models.cat.Body.Elytra.left)
-        vt.restorePart(models.cat.Body.Elytra.right)
-    end
 end
 modules.events.POST_RENDER:register(vt.postRender)
 
@@ -66,6 +76,13 @@ end
 function vt.restorePart(modelPart)
     modelPart:setPos(vt.originalPos[modelPart])
     modelPart:setRot(vt.originalRot[modelPart])
+end
+
+function vt.restoreVanillaParts(...)
+    for _, vanillaPart in pairs({...}) do
+        vanillaPart:setPos()
+        vanillaPart:setRot()
+    end
 end
 
 return vt
