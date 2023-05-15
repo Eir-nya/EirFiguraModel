a06edd1c6b4917d0e2ef3517021c744aec3738cd
Eir-nya <Eir.nyanbinary@gmail.com>
2023-05-15 18:57:40 -0500

    Update thighs.lua

--- scripts/modules/thighs.lua
+++ scripts/modules/thighs.lua
@@ -3,8 +3,26 @@ modules.events.RENDER:register(function(delta, ctx)
 		return
 	end
 
-	models.cat.RightThigh:setPos(vanilla_model.RIGHT_LEG:getOriginPos() + vanilla_model.RIGHT_LEG:getPos())
-	models.cat.LeftThigh:setPos(vanilla_model.LEFT_LEG:getOriginPos() + vanilla_model.LEFT_LEG:getPos())
+	local vanillaRightPos = vanilla_model.RIGHT_LEG:getOriginPos()
+	if vanilla_model.RIGHT_LEG:getPos() then
+		vanillaRightPos = vanillaRightPos + vanilla_model.RIGHT_LEG:getPos()
+	end
+	local vanillaLeftPos = vanilla_model.LEFT_LEG:getOriginPos()
+	if vanilla_model.LEFT_LEG:getPos() then
+		vanillaLeftPos = vanillaLeftPos + vanilla_model.LEFT_LEG:getPos()
+	end
+	local vanillaRightRot = vanilla_model.RIGHT_LEG:getOriginRot()
+	if vanilla_model.RIGHT_LEG:getRot() then
+		vanillaRightRot = vanillaRightRot + vanilla_model.RIGHT_LEG:getRot()
+	end
+	local vanillaLeftRot = vanilla_model.LEFT_LEG:getOriginRot()
+	if vanilla_model.RIGHT_LEG:getRot() then
+		vanillaLeftRot = vanillaLeftRot + vanilla_model.LEFT_LEG:getRot()
+	end
+
+	
+	models.cat.RightThigh:setPos(vanillaRightPos)
+	models.cat.LeftThigh:setPos(vanillaLeftPos)
 
 	if previous.pose == "CROUCHING" then
 		models.cat.RightThigh:setPos(models.cat.RightThigh:getPos() + vec(0, 0, 1.5))
@@ -13,8 +31,8 @@ modules.events.RENDER:register(function(delta, ctx)
 		models.cat.LeftThigh:setRot(vanilla_model.BODY:getOriginRot() / 1.5)
 	else
 		if settings.model.vanillaMatch then
-			models.cat.RightThigh:setRot((vanilla_model.RIGHT_LEG:getOriginRot() + vanilla_model.RIGHT_LEG:getRot()) / 2)
-			models.cat.LeftThigh:setRot((vanilla_model.LEFT_LEG:getOriginRot() + vanilla_model.LEFT_LEG:getRot()) / 2)
+			models.cat.RightThigh:setRot(vanillaRightRot / 2)
+			models.cat.LeftThigh:setRot(vanillaLeftRot / 2)
 		else
 			models.cat.RightThigh:setRot(vanilla_model.RIGHT_LEG:getOriginRot() + models.cat.RightLeg:getTrueRot() / 2)
 			models.cat.LeftThigh:setRot(vanilla_model.LEFT_LEG:getOriginRot() + models.cat.LeftLeg:getTrueRot() / 2)
