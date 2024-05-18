15c3a1453bd851780000a97c9e139592aab488be
Eir <eir.nyanbinary@gmail.com>
2024-05-18 14:15:09 -0500

    Try to make data sync less intense

--- scripts/modules/sync.lua
+++ scripts/modules/sync.lua
@@ -1,10 +1,11 @@
--- Library that pings data every 20 seconds to keep other clients up-to-date with host-only data when joining or reloading this avatar.
+-- Library that pings data every 25 seconds to keep other clients up-to-date with host-only data when joining or reloading this avatar.
 local sync = {
 	timer = 1,
-	maxTimer = 20 * 20,
+	maxTimer = 25 * 20,
+	toggle = false
 }
 
-function sync.DoSync()
+function sync.DoSync1()
 	pings.setArmorVisible(modules.armor.display)
 	for slot, clothes in pairs(modules.clothes) do
 		if type(clothes) == "table" then
@@ -13,9 +14,12 @@ function sync.DoSync()
 	end
 	pings.setXP(previous.xp)
 	pings.setEffects(previous.effects)
+end
+
+function sync.DoSync2()
 	pings.setFlying(previous.flying)
 	pings.setAir(previous.airPercent)
-	pings.setNearest(modules.eyes.nearest)
+	-- pings.setNearest(modules.eyes.nearest)
 	modules.eyes.checkNightVision()
 	modules.eyes.checkScaredEffects()
 	if modules.sit.isSitting then
@@ -27,8 +31,13 @@ if host:isHost() then
 	modules.events.TICK:register(function()
 		sync.timer = sync.timer - 1
 		if sync.timer == 0 then
+			sync.toggle = not sync.toggle
 			sync.timer = sync.maxTimer
-			sync.DoSync()
+			if sync.toggle then
+				sync.DoSync1()
+			else
+				sync.DoSync2()
+			end
 		end
 	end)
 end
